1d0a91509d63ee0758a9da39b6e2c88c632b9bb6
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-11-06 18:02:50 -0300

    Utilizando o Metadados no projeto e montando a página de Customers

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,10 +4,13 @@ import Cards from "@/app/ui/dashboard/cards";
 import { lusitana } from "../../ui/fonts";
 import { Suspense } from "react";
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from "@/app/ui/skeletons";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Dashboard',
+}
 
 export default async function Page() {
-    // const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} = await fetchCardData();
-
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
